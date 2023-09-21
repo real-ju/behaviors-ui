ed62eb085f6cc11f4de2095cd634e412149e1e6e
real-ju <2201181566@qq.com>
2023-09-21 11:13:21 +0800

    fix bug

---
+++ example/assets/pages-icon-basic.df81f43a.js
@@ -0,0 +1,1 @@
+import{B as a}from"./index.7dcf5b2c.js";import{_ as s,o as e,c as o,w as c,i as n,a as r,r as i,d as t}from"./index-0f8f99ee.js";const d=s({},[["render",function(s,d){const p=i(t("be-icon"),a),l=n;return e(),o(l,{class:"playground icon-basic-page"},{default:c((()=>[r(p,{name:"plus-circle"})])),_:1})}],["__scopeId","data-v-813aec26"]]);export{d as default};
