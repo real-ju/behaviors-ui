68744769c30bc10e6d75035b7b1e3c68eaebf8d3
real-ju <2201181566@qq.com>
2024-04-19 16:56:30 +0800

    更新文档

--- example/assets/pages-icon-basic.b952005c.js
+++
@@ -1,1 +0,0 @@
-import{B as a}from"./index.b1d1cd92.js";import{_ as s,o as e,c as o,w as c,i as n,a as r,r as i,d as t}from"./index-ca8328b0.js";const d=s({},[["render",function(s,d){const p=i(t("be-icon"),a),l=n;return e(),o(l,{class:"playground icon-basic-page"},{default:c((()=>[r(p,{name:"plus-circle"})])),_:1})}],["__scopeId","data-v-813aec26"]]);export{d as default};
---
+++ example/assets/pages-icon-basic.d0ba1078.js
@@ -0,0 +1,1 @@
+import{B as a}from"./index.eb225935.js";import{_ as s,o as e,c as o,w as c,i as n,a as r,r as i,d as t}from"./index-075d9373.js";const d=s({},[["render",function(s,d){const p=i(t("be-icon"),a),l=n;return e(),o(l,{class:"playground icon-basic-page"},{default:c((()=>[r(p,{name:"plus-circle"})])),_:1})}],["__scopeId","data-v-813aec26"]]);export{d as default};
